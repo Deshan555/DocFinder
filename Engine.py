d9bee9970361f7d8c380208d2dffcad9da9c1dc0
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-08-10 16:48:32 +0530

    finish new index method

--- Engine.py
+++ Engine.py
@@ -16,4 +16,16 @@ class file_finder:
 
         self.records = 0
 
+    def new_index(self, root_path):
+
+        """" create new index file and save that file """
+
+        self.file_index = [(root, files)for root, dirs, files in os.walk(root_path) if files]
+
+        """" save that file with details"""
+
+        with open('file_index.pkl', 'wb') as source_file:
+
+            pickle.dump(self.file_index, source_file)
+
 
