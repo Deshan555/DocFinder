ee354ba6ab06e6506f881dee609bdc9174d4b4ac
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-08-10 17:47:25 +0530

    finalize search engine

--- Engine.py
+++ Engine.py
@@ -36,7 +36,7 @@ class file_finder:
         except:
             self.file_index = []
 
-    def search(self, term, search_options = 'contains'):
+    def search(self, term, search_options='contains'):
 
         self.results.clear()
 
@@ -44,3 +44,39 @@ class file_finder:
 
         self.records = 0
 
+        for path, files in self.file_index:
+
+            for file in files:
+
+                self.records += 1
+
+                if (search_options == 'contains' and term.lower() in file.lower() or
+                        search_options == 'startswith' and file.lower().startswith(term.lower()) or
+                        search_options == 'endswith' and file.lower().endswith(term.lower())):
+
+                    result = path.replace('\\', '/') + '/' + file
+
+                    self.results.append(result)
+
+                    self.matches += 1
+
+                else:
+                    continue
+
+        with open('search_results.txt', 'w') as file_source:
+
+            for row in self.results:
+
+                file_source.write(row + '\n')
+
+
+
+def test_file():
+
+    frozen = file_finder()
+
+    frozen.new_index('c:/')
+
+    frozen.search('Android')
+
+test_file()
